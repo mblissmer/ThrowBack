729bd152d889ad7bafa1168b7bbd1569126e153d
Matt Blissmer <mattblissmer@gmail.com>
2017-11-29 20:52:15 -0500

    fixed resolution issues. tested. submitted. FINISHED.

--- ViewportControl.gd
+++ ViewportControl.gd
@@ -4,9 +4,13 @@ onready var viewport = get_viewport()
 
 func _ready():
 	var monSize = OS.get_screen_size()
-	print("Size: " + str(monSize))
-	print("DPI: " + str(OS.get_screen_dpi()))
 	if monSize == Vector2(1920,1080):
+		OS.set_window_fullscreen(true)
+	elif monSize == Vector2(1600,900):
+		OS.set_window_size(monSize)
+		OS.set_window_fullscreen(true)
+	elif monSize == Vector2(1280,720):
+		OS.set_window_size(monSize)
 		OS.set_window_fullscreen(true)
 	else:
 		OS.set_window_size(Vector2(1024,576))
