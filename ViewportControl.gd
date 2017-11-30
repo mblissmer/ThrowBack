95f7982a8c8840318d0795ee124061ee4317d804
Matt <mattblissmer@gmail.com>
2017-11-30 11:07:56 -0500

    fixed issue with resolution in web player

--- ViewportControl.gd
+++ ViewportControl.gd
@@ -3,15 +3,21 @@ extends Control
 onready var viewport = get_viewport()
 
 func _ready():
-	var monSize = OS.get_screen_size()
-	if monSize == Vector2(1920,1080):
-		OS.set_window_fullscreen(true)
-	elif monSize == Vector2(1600,900):
-		OS.set_window_size(monSize)
-		OS.set_window_fullscreen(true)
-	elif monSize == Vector2(1280,720):
-		OS.set_window_size(monSize)
-		OS.set_window_fullscreen(true)
+	if OS.get_name() == "HTML5":
+		defaultView()
 	else:
-		OS.set_window_size(Vector2(1024,576))
-		OS.set_window_position(Vector2(10,10))
+		var monSize = OS.get_screen_size()
+		if monSize == Vector2(1920,1080):
+			OS.set_window_fullscreen(true)
+		elif monSize == Vector2(1600,900):
+			OS.set_window_size(monSize)
+			OS.set_window_fullscreen(true)
+		elif monSize == Vector2(1280,720):
+			OS.set_window_size(monSize)
+			OS.set_window_fullscreen(true)
+		else:
+			defaultView()
+
+func defaultView():
+	OS.set_window_size(Vector2(1024,576))
+	OS.set_window_position(Vector2(10,10))
